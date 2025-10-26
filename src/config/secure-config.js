90c3beb735260d6c5a6144631e3f0a4e7e01c4f6
rynathusofficialgaming-bit <rynathusofficialgaming@gmail.com>
2025-10-26 18:53:27 +0000

    Update secure-config.js

--- src/config/secure-config.js
+++ src/config/secure-config.js
@@ -6,11 +6,19 @@ export const SECURE_CONFIG = {
   ],
   
   midiLinks: {
-    "Hip Hop": {
-      "C Major - A Minor": [
-        "https://www.dropbox.com/scl/fi/7y7ttwrzf1pkz4xy2keyv/midi1.mid?rlkey=hy5cbfea5ioolvmh5bkmp7gdh&st=ynu9cu4z&dl=1",
-        "https://www.dropbox.com/scl/fi/7y7ttwrzf1pkz4xy2keyv/midi1.mid?rlkey=hy5cbfea5ioolvmh5bkmp7gdh&st=ynu9cu4z&dl=1",
-        "https://www.dropbox.com/scl/fi/7y7ttwrzf1pkz4xy2keyv/midi1.mid?rlkey=hy5cbfea5ioolvmh5bkmp7gdh&st=ynu9cu4z&dl=1"
+    "House": {
+      "A Major - F# Minor": [
+        "https://www.dropbox.com/scl/fi/7y7ttwrzf1pkz4xy2keyv/midi1.mid?rlkey=hy5cbfea5ioolvmh5bkmp7gdh&st=7hgi3j2f&dl=1",
+        "https://www.dropbox.com/scl/fi/koxsvx510yr6yc79aw1zy/midi2.mid?rlkey=nguo29cwrunkwvn7lmnrhrlre&st=o7gqf82d&dl=1",
+        "https://www.dropbox.com/scl/fi/t50p6913w8eqljjvb3v34/midi4.mid?rlkey=106nsg23bwl5uyjqzkx1fmrn9&st=bilb6c9r&dl=1",
+        "https://www.dropbox.com/scl/fi/vykxs9v8dgzd55n7kqpsz/midi5.mid?rlkey=h9z6bw0muzozw9q9mwu4cotql&st=1iiopjci&dl=1",
+        "https://www.dropbox.com/scl/fi/1q53v0fqzr4ptx6ap4zy3/midi6.mid?rlkey=1vm9n8iaakkgqjna63kx0c2ex&st=dz3gsl4p&dl=1",
+        "https://www.dropbox.com/scl/fi/u40f40ga7xfz4teq6ki6c/midi7.mid?rlkey=rp9ggl7h3gzmzjpcascznf95e&st=778s6mas&dl=1",
+        "https://www.dropbox.com/scl/fi/tddoqfjbidr4hi1kg2rff/midi8.mid?rlkey=n01w5e7mul8x8lenah7mjmrgg&st=vg25nx6e&dl=1",
+        "https://www.dropbox.com/scl/fi/4ks7nn807mon7ni63h9kj/midi9.mid?rlkey=wem9p42r4kh09rfb6hs5ifrrr&st=40hvgdby&dl=1",
+        "https://www.dropbox.com/scl/fi/iy665n6jtnhl5zzzsdkn4/midi10.mid?rlkey=4sc5mmi6s70nmoalbo23jzdfe&st=j7beb8dd&dl=1",
+        "https://www.dropbox.com/scl/fi/ad9tquig98j8xhyg4c1so/midi11.mid?rlkey=qqtwtfb7qhuqzo1d359kipm4w&st=w9d8w050&dl=1",
+        "https://www.dropbox.com/scl/fi/bbf4ra9vf0xorasbprn66/midi3.mid?rlkey=6z1hf86dj1x6e043gf4xatkgd&st=ictipub7&dl=1"
       ],
       "G Major - E Minor": [
         "https://www.dropbox.com/s/example_hiphop_gmajor_1/hiphop-gmajor-1.mid?dl=1"
